23bdae247928bb8c468198401636b14054c1a5db
maxioul <maxiou7391468250@126.com>
2023-12-09 08:24:31 +0900

    Change to typescript

--- src/main/java/dean/gymtrack/Controller/HomeController.java
+++ src/main/java/dean/gymtrack/Controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    @RequestMapping(value = "/gymtrack")
     public String forward(){
         return "forward:/";
     }
